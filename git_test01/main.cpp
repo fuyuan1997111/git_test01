7d36eccfa4898b61f96422d04bde266d8abb6220
ieg123 <154439441@qq.com>
2019-11-07 15:07:26 +0800

    sds

---
+++ git_test01/main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unordered_map>
+#include <map>
+#include <string>
+
+int main()
+{
+	std::unordered_map<int, std::string> _map;
+	
+	_map[1] = "a";
+	_map[2] = "b";
+
+	_map[4] = "c";
+	_map[5] = "d";
+	std::unordered_map<int, std::string >::iterator _iter=_map.find(3);
+
+	if (_iter == _map.end())
+		printf("1\n");
+	else
+		printf("0\n");
+
+	return 0;
+}
